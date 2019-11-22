aa00815721e9ce328c6e61067b215619d99c343a
Alyssa Yates <bosslys@icloud.com>
2019-11-22 19:56:00 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,11 @@ end
 
 # If we had a `User` model/controller, in `routes.rb` file, you would add the
 # following line:
-# 
+#
 # ```ruby
 # get '/students/:id/activate', to: 'users#new', as: 'register'
 # ```
-# 
+#
 # Now the application lets programmers use `register_path` when creating links
 # with `link_to`. Rails leverages routes and these "helper route" names in many
 # places to help you keep your code flexible and brief.
