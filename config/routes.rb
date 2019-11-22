ac7d8cbe53bfbe06920c9ed501d9672e432e4882
Alyssa Yates <bosslys@icloud.com>
2019-11-22 19:55:58 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :students, only: [:index, :show]
+  resources :students, only: [:index, :show, :activate]
 end
+
+
+# If we had a `User` model/controller, in `routes.rb` file, you would add the
+# following line:
+# 
+# ```ruby
+# get '/students/:id/activate', to: 'users#new', as: 'register'
+# ```
+# 
+# Now the application lets programmers use `register_path` when creating links
+# with `link_to`. Rails leverages routes and these "helper route" names in many
+# places to help you keep your code flexible and brief.
